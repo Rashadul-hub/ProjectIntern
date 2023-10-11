55cf39959eda6f2cd1414ce8a8a9144f1c605ef3
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-10-12 03:38:33 +0600

    Updated With Theme Mode Switching

---
+++ app/src/main/java/com/example/projectintern/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.projectintern
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectintern.app.ProjectApp
+import com.example.projectintern.composed.ThemeSwitch
+
+
+var darkMode  by mutableStateOf(true) //For// Switching Theme Dark -> Light Mode
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectApp()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ViewMain() {
+    ProjectApp()
+
+}
