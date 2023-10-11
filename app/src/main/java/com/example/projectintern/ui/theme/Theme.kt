b9d31b8d7ae1e72e6c86f53cd9e142e7f67f255f
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-10-12 03:43:42 +0600

    Modify Theme File
    Added Responsive Screen Size
    Modify Theme Switching
    Added Object AppTheme for Padding

---
+++ app/src/main/java/com/example/projectintern/ui/theme/Theme.kt
@@ -0,0 +1,151 @@
+package com.example.projectintern.ui.theme
+
+import android.app.Activity
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import com.example.projectintern.model.Dimensions
+import com.example.projectintern.model.WindowSize
+import com.example.projectintern.model.WindowSizeClass
+import com.example.projectintern.model.compactDimensions
+import com.example.projectintern.model.largeDimensions
+import com.example.projectintern.model.mediumDimensions
+import com.example.projectintern.model.smallDimensions
+import com.example.projectintern.utils.LocalAppDimens
+import com.example.projectintern.utils.LocalOrientationMode
+import com.example.projectintern.utils.Orientation
+import com.example.projectintern.utils.ProvideAppUtils
+
+
+private val DarkColorPalette = darkColorScheme(
+    primary = PrimaryDark,
+    onPrimary = OnPrimaryDark,
+    primaryContainer = PrimaryContainerDark,
+    onPrimaryContainer = OnPrimaryContainerDark,
+    secondary = SecondaryDark,
+    onSecondary = OnSecondaryDark,
+    secondaryContainer = SecondaryContainerDark,
+    onSecondaryContainer = OnSecondaryContainerDark,
+    tertiary = TertiaryDark,
+    onTertiary = OnTertiaryDark,
+    tertiaryContainer = TertiaryContainerDark,
+    onTertiaryContainer = OnTertiaryContainerDark,
+    error = ErrorDark,
+    onError = OnErrorDark,
+    errorContainer = ErrorContainerDark,
+    onErrorContainer = OnErrorContainerDark,
+    background = BackgroundDark,
+    onBackground = OnBackgroundDark,
+    surface = SurfaceDark,
+    outline = OutlineDark,
+    surfaceVariant = SurfaceVariantDark,
+    onSurfaceVariant = OnSurfaceVariantDark,
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = PrimaryLight,
+    onPrimary = OnPrimaryLight,
+    primaryContainer = PrimaryContainerLight,
+    onPrimaryContainer = OnPrimaryContainerLight,
+    secondary = SecondaryLight,
+    onSecondary = OnSecondaryLight,
+    secondaryContainer = SecondaryContainerLight,
+    onSecondaryContainer = OnSecondaryContainerLight,
+    tertiary = TertiaryLight,
+    onTertiary = OnTertiaryLight,
+    tertiaryContainer = TertiaryContainerLight,
+    onTertiaryContainer = OnTertiaryContainerLight,
+    error = ErrorLight,
+    onError = OnErrorLight,
+    errorContainer = ErrorContainerLight,
+    onErrorContainer = OnErrorContainerLight,
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+    surface = SurfaceLight,
+    outline = OutlineLight,
+    surfaceVariant = SurfaceVariantLight,
+    onSurfaceVariant = OnSurfaceVariantLight,
+)
+
+@Composable
+fun ProjectInternTheme(
+
+    windowSizeClass: WindowSizeClass, //Responsive Screen Size
+
+    darkTheme: Boolean ,
+    //= isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+
+    val colors = if (darkTheme){
+        DarkColorPalette
+    }else{
+        LightColorPalette
+    }
+
+    //Optional , This Part helps You Set The Status-bar Color
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb()
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+    //For Responsive Screen Size
+    val orientation = when{
+        windowSizeClass.width.size > windowSizeClass.height.size -> Orientation.Landscape
+        else -> Orientation.Portrait
+    }
+
+    val sizeThatMatters = when(orientation){
+        Orientation.Portrait -> windowSizeClass.width
+        else -> windowSizeClass.height
+    }
+
+    val dimensions = when(sizeThatMatters){
+        is WindowSize.Small -> smallDimensions
+        is WindowSize.Compact -> compactDimensions
+        is WindowSize.Medium -> mediumDimensions
+        else -> largeDimensions
+    }
+
+    val typography = when(sizeThatMatters){
+        is WindowSize.Small -> typographySmall
+        is WindowSize.Compact -> typographyCompact
+        is WindowSize.Medium -> typographyMedium
+        else -> typographyBig
+    }
+
+
+    //AppUtils Class
+    ProvideAppUtils(dimensions = dimensions, orientation = orientation) {
+
+        MaterialTheme(
+            colorScheme = colors,
+            typography = typography,
+            content = content
+        )
+    }
+}
+
+// For Theme Padding
+
+object AppTheme{
+    val dimens : Dimensions
+        @Composable
+        get() = LocalAppDimens.current
+
+    val orientation : Orientation
+        @Composable
+        get() = LocalOrientationMode.current
+}
