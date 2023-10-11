fe15278beaba327324d2d655d362fbc88d2b55fa
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-10-12 05:04:31 +0600

    New Composable Function Added

--- app/src/main/java/com/example/projectintern/composed/ComposableFunctions.kt
+++ app/src/main/java/com/example/projectintern/composed/ComposableFunctions.kt
@@ -1,6 +1,8 @@
 package com.example.projectintern.composed
 
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,15 +10,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -24,8 +35,13 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.projectintern.R
 import com.example.projectintern.darkMode
 import com.example.projectintern.ui.theme.AppTheme
+import com.example.projectintern.ui.theme.ComponentShapes
+import com.example.projectintern.ui.theme.PrimaryLight
+import com.example.projectintern.ui.theme.SecondaryContainerLight
+import com.example.projectintern.ui.theme.SurfaceDark
 
 @Composable
 fun TextHeadLine(text: String, color: Color) {
@@ -86,12 +102,62 @@ fun ThemeSwitch() {
 fun NormalTextComponent(value: String) {
     Text(
         text = value,
-        modifier = Modifier.fillMaxWidth().heightIn(min = 80.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
         style = TextStyle(
-            fontSize =  18.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal
-        )
+        ),
+        color = SurfaceDark,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun HeadingTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier.fillMaxWidth(),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        ),
+        color = SurfaceDark,
+        textAlign = TextAlign.Center
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextFieldComponent(labelValue: String) {
+
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(ComponentShapes.small),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = PrimaryLight,
+            focusedLabelColor = PrimaryLight,
+            cursorColor = PrimaryLight,
+            containerColor = SecondaryContainerLight
+        ),
+        keyboardOptions = KeyboardOptions.Default,
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+
+        leadingIcon = {
+            Icon(painter = painterResource(id = R.drawable.profile), contentDescription = "")
+        }
     )
 }
 
