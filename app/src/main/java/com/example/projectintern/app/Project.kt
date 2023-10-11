d1ecbcf13f5dc4e2ad7f5d74a9652c403c4178b0
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-10-12 03:37:48 +0600

    Create Main Project File
    This File Contains All The screens

---
+++ app/src/main/java/com/example/projectintern/app/Project.kt
@@ -0,0 +1,53 @@
+package com.example.projectintern.app
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectintern.composed.ThemeSwitch
+import com.example.projectintern.darkMode
+import com.example.projectintern.model.rememberWindowSizeClass
+import com.example.projectintern.screens.LandscapeScreen
+import com.example.projectintern.screens.PortraitScreen
+import com.example.projectintern.ui.theme.AppTheme
+import com.example.projectintern.ui.theme.ProjectInternTheme
+import com.example.projectintern.utils.LanguageSelection
+import com.example.projectintern.utils.Orientation
+
+
+/**This File Contains All The Screens**/
+@Composable
+fun ProjectApp() {
+
+    val context = LocalContext.current
+    var selectedLanguage by remember { mutableStateOf(LanguageSelection.ENGLISH) }
+
+    val window = rememberWindowSizeClass()
+    ProjectInternTheme(window, darkTheme = darkMode) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+
+            if (AppTheme.orientation == Orientation.Portrait) {
+                PortraitScreen()
+            } else {
+                LandscapeScreen()
+            }
+        }
+
+
+    }
+
+}
+
+@Preview
+@Composable
+fun ProjectPreview() {
+    ProjectApp()
+}
