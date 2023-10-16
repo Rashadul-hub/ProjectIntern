591ec6f546fce35be4ecb609abd26f3dd8d232d0
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-10-16 16:50:43 +0600

    Updated

--- app/src/main/java/com/example/projectintern/model/Demensions.kt
+++ app/src/main/java/com/example/projectintern/model/Demensions.kt
@@ -3,44 +3,44 @@ package com.example.projectintern.model
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-data class Dimensions(
-    val small:Dp,
-    val smallMedium:Dp,
-    val medium:Dp,
-    val mediumLarge:Dp,
-    val large: Dp
 
+
+data class Dimensions(
+    val small: Dp,
+    val smallMedium: Dp,
+    val medium: Dp,
+    val mediumLarge: Dp,
+    val large: Dp
 )
 
-
 val smallDimensions = Dimensions(
-    small = 2.dp,
-    smallMedium = 4.dp,
-    medium = 6.dp,
-    mediumLarge = 9.dp,
-    large = 13.dp
+    small = 20.dp,
+    smallMedium = 25.dp,
+    medium = 7.dp,
+    mediumLarge = 40.dp,
+    large = 46.dp
 )
 
 val compactDimensions = Dimensions(
-    small = 3.dp,
-    smallMedium = 5.dp,
-    medium = 8.dp,
-    mediumLarge = 11.dp,
-    large = 15.dp
+    small = 30.dp,
+    smallMedium = 35.dp,
+    medium = 10.dp,
+    mediumLarge = 45.dp,
+    large = 55.dp
 )
 
 val mediumDimensions = Dimensions(
-    small = 5.dp,
-    smallMedium = 7.dp,
-    medium = 10.dp,
-    mediumLarge = 13.dp,
-    large = 18.dp
+    small = 40.dp,
+    smallMedium = 45.dp,
+    medium = 15.dp,
+    mediumLarge = 50.dp,
+    large = 60.dp
 )
 
 val largeDimensions = Dimensions(
-    small = 8.dp,
-    smallMedium = 11.dp,
-    medium = 15.dp,
-    mediumLarge = 20.dp,
-    large = 25.dp
+    small = 50.dp,
+    smallMedium = 55.dp,
+    medium = 20.dp,
+    mediumLarge = 60.dp,
+    large = 70.dp
 )
