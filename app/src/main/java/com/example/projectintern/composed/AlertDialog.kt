a6d37386c43aec4b2492b38a2ad1f0e396dc856f
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-10-23 03:02:04 +0600

    Modified -> Composable

--- app/src/main/java/com/example/projectintern/composed/AlertDialog.kt
+++ app/src/main/java/com/example/projectintern/composed/AlertDialog.kt
@@ -10,29 +10,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.CancelPresentation
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.projectintern.R
 
 @Composable
-fun AlertDialog() {
+fun AlertDialog(navController: NavController) {
 
     Dialog(onDismissRequest = { /*TODO*/ }) {
         Surface(shape = RoundedCornerShape(14.dp), color = MaterialTheme.colors.background)
@@ -46,58 +49,60 @@ fun AlertDialog() {
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
+
                         Text(
-                            text = "ERROR", style = TextStyle(
-                                fontSize = 20.sp,
-                                fontFamily = FontFamily.Default,
-                                fontWeight = FontWeight.Bold,
-                                color = MaterialTheme.colors.primary
+                            text = stringResource(id = R.string.error),
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                letterSpacing = 3.sp,
+                                fontFamily = FontFamily(Font(R.font.inter_semi_bold)),
+                                fontWeight = FontWeight(700),
+                                color = Color.DarkGray
                             )
                         )
-//                        Icon(
-//                            imageVector = Icons.Filled.Cancel,
-//                            contentDescription = "",
-//                            tint = Color.DarkGray,
-//                            modifier = Modifier
-//                                .clip(shape = CircleShape)
-//                                .width(30.dp)
-//                                .height(30.dp)
-//                                .clickable {
-//
-//                                    ///Actions
-//
-//
-//                                })
+                        Icon(
+                            imageVector = Icons.Filled.CancelPresentation,
+                            contentDescription = "",
+
+                            tint = Color.DarkGray,
+                            modifier = Modifier
+                                .width(20.dp)
+                                .height(20.dp)
+                                .clickable {
+
+                                    ///used FOr Demo Purpose
+                                    navController.navigate("terms")
+
+
+                                })
+
 
                     }
                     Spacer(modifier = Modifier.height(15.dp))
 
-
-
                     Text(
-                        text = " Type your Error Message", style = TextStyle(
+                        text = stringResource(id = R.string.invalid_action),
+                        style = TextStyle(
                             fontSize = 15.sp,
-                            color = MaterialTheme.colors.error
-                        )
+                            color = MaterialTheme.colors.error,
+                            fontFamily = FontFamily(Font(R.font.inter_regular)),
+                            fontWeight = FontWeight(500),
+
+                            )
                     )
                     Spacer(modifier = Modifier.height(15.dp))
 
                     Box(
                         modifier = Modifier.padding(
                             start = 30.dp,
-                            top = 0.dp,
                             end = 30.dp,
-                            bottom = 0.dp
                         )
                     ) {
 
-                        CustomButton(buttonText = "Okay",onClick = {
-
-                            //Action
-                        })
-                            
-
-
+                        CustomButton(buttonText = "Okay") {
+                            ///used FOr Demo Purpose
+                            navController.navigate("mode")
+                        }
 
 
                     }
@@ -105,8 +110,6 @@ fun AlertDialog() {
                 }
 
             }
-
-
         }
     }
 }
@@ -114,5 +117,6 @@ fun AlertDialog() {
 @Preview
 @Composable
 fun AlertDialogView() {
-    AlertDialog()
+    val dummyNavController = rememberNavController() // Create a dummy NavController
+    AlertDialog(navController = dummyNavController)
 }
